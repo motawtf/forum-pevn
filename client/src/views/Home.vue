5d784618d6f9feb45d162e83f9f528971d566fee
MotaWTF <sergiomota182@gmail.com>
2019-02-28 11:11:17 +0100

    Initial Commit!

---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to COmmunity!</h1>
+      <p class="lead">A fully open source forum. Made with love by Mota</p>
+      <hr class="my-4">
+      <p>This forum is all about community. Be kind! Be helpful!</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
